0665ef677bd5c8c409fca5628fd106c2983d559c
ateals <ateals@icloud.com>
2023-08-11 22:06:17 +0900

    github pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,21 +4,26 @@ import Layout from "../Layout";
 import DD from "@/pages/DD";
 import DragDrop from "@/pages/DragDrop";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                {
+                    path: "/",
+                    element: <DD />,
+                },
+                {
+                    path: "/dnd",
+                    element: <DragDrop />,
+                },
+            ],
+        },
+    ],
     {
-        path: "/",
-        element: <Layout />,
-        children: [
-            {
-                path: "/",
-                element: <DD />,
-            },
-            {
-                path: "/dnd",
-                element: <DragDrop />,
-            },
-        ],
-    },
-]);
+        basename: "/react-todolist-recoil/",
+    }
+);
 
 export default router;
